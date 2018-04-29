9cedce1d60fea3794261a3ae290a4596c477d310
Ayoosh Kathuria <kathuria.ayoosh@gmail.com>
2018-04-29 17:01:39 +0530

    Still hate PyTorch. Someone fix this CUDA shit.

--- util.py
+++ util.py
@@ -34,8 +34,11 @@ def bbox_iou(box1, box2):
     inter_rect_y2 =  torch.min(b1_y2, b2_y2)
     
     #Intersection area
-    inter_area = torch.max(inter_rect_x2 - inter_rect_x1 + 1,inter_rect_x2.new(torch.zeros(inter_rect_x2.shape)))*torch.max(inter_rect_y2 - inter_rect_y1 + 1, inter_rect_x2.new(torch.zeros(inter_rect_x2.shape)))
+    inter_area = torch.max(inter_rect_x2 - inter_rect_x1 + 1,torch.zeros(inter_rect_x2.shape))*torch.max(inter_rect_y2 - inter_rect_y1 + 1, torch.zeros(inter_rect_x2.shape))
     
+    if torch.cuda.is_available():
+            inter_area = torch.max(inter_rect_x2 - inter_rect_x1 + 1,torch.zeros(inter_rect_x2.shape).cuda())*torch.max(inter_rect_y2 - inter_rect_y1 + 1, torch.zeros(inter_rect_x2.shape).cuda())
+
     #Union Area
     b1_area = (b1_x2 - b1_x1 + 1)*(b1_y2 - b1_y1 + 1)
     b2_area = (b2_x2 - b2_x1 + 1)*(b2_y2 - b2_y1 + 1)
